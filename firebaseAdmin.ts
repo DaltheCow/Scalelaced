341f5a4c9c92548d47a7045861bcc0af6aca3c66
Ben Blue <dalcowboiz@gmail.com>
2021-08-05 11:45:24 -0400

    fix

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -6,7 +6,10 @@ if (!firebaseAdmin.apps.length) {
       type: `${process.env.NEXT_PUBLIC_FIREBASE_TYPE}`,
       project_id: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}`,
       private_key_id: `${process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY_ID}`,
-      private_key: `${process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY}`,
+      private_key: `${process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY?.replace(
+        /\\n/g,
+        "\n"
+      )}`,
       client_email: `${process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL}`,
       client_id: `${process.env.NEXT_PUBLIC_FIREBASE_CLIENT_ID}`,
       auth_uri: `${process.env.NEXT_PUBLIC_FIREBASE_AUTH_URI}`,
