279ce101e58ee2799a5dc43d93da8fb91f3701b5
Ben Blue <dalcowboiz@gmail.com>
2021-08-05 14:00:21 -0400

    fix

--- firebaseClient.ts
+++ firebaseClient.ts
@@ -7,6 +7,13 @@ const CLIENT_CONFIG = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 };
 
+console.log("here");
+console.log({
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+});
+
 if (typeof window !== "undefined" && !firebaseClient.apps.length) {
   firebaseClient.initializeApp(CLIENT_CONFIG);
   firebaseClient
