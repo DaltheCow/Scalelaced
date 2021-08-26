30b59dd3e560064e8035eff467e023c43efa79e0
Ben Blue <dalcowboiz@gmail.com>
2021-08-26 12:25:56 -0600

    fix build

--- components/SubmissionModal.tsx
+++ components/SubmissionModal.tsx
@@ -1,7 +1,8 @@
 import { Modal } from "@material-ui/core";
 
 const SubmissionModal = () => {
-  return <Modal></Modal>;
+  // return <Modal></Modal>;
+  return <></>;
 };
 
 export default SubmissionModal;
